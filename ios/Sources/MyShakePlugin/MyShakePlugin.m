9d87343196e3e507525c8a247208550a4d25eb78
Ravi Jatav <j.ravi@wenodo.com>
2026-02-23 18:13:30 +0530

    fix: simplified ios structure to 2 files

--- ios/Sources/MyShakePlugin/MyShakePlugin.m
+++ ios/Sources/MyShakePlugin/MyShakePlugin.m
@@ -1,6 +1,8 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
+// Pehla parameter: MyShakePlugin (Swift class name)
+// Dusra parameter: MyShake (JS registerPlugin name)
 CAP_PLUGIN(MyShakePlugin, "MyShake",
            CAP_PLUGIN_METHOD(addListener, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnNone);
--- ios/Sources/MyShakePlugin/MyShakePlugin.swift
+++ ios/Sources/MyShakePlugin/MyShakePlugin.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Capacitor
 
+// 1. Native UIWindow extension (Native Shake detection)
 extension UIWindow {
     open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
@@ -13,13 +14,20 @@ extension Notification.Name {
     static let deviceDidShake = Notification.Name("deviceDidShake")
 }
 
+// 2. Plugin Bridge Class
 @objc(MyShakePlugin)
 public class MyShakePlugin: CAPPlugin {
+    
     override public func load() {
+        // Listen for native notification
         NotificationCenter.default.addObserver(self, selector: #selector(self.onShakeDetected), name: .deviceDidShake, object: nil)
     }
 
     @objc private func onShakeDetected() {
+        // Yeh line Xcode terminal mein dikhegi
+        print("DEBUG: Native Shake Captured - Sending to JS")
+        
+        // JS ko notify hamesha MAIN THREAD se karein
         DispatchQueue.main.async {
             self.notifyListeners("shake", data: [:])
         }
